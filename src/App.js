396892b3abbf553911dd3480c03416c132c46849
Kurt Johnson <fazbat@gmail.com>
2018-08-22 18:20:48 -0700

    add react router, MaterialUI, and template home,dashboard, and settings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/nav-bar";
+import Home from "./components/home";
+import Dashboard from "./components/dashboard";
+import Settings from "./components/settings";
 
 class App extends Component {
   render() {
@@ -9,9 +13,10 @@ class App extends Component {
         <header className="App-header">
           <NavBar />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="spacer" style={{ marginTop: 65 + "px" }} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/settings" component={Settings} />
       </div>
     );
   }
---
+++ src/components/settings.js
@@ -0,0 +1,8 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Settings = () => {
+  return <span>Settings</span>;
+};
+
+export default Settings;
